a4ef6ed97865da86f74f3f84dcf4bd37efc2779b
DhruvPopat268 <dhruvpopat268@gmail.com>
2026-02-02 13:23:51 +0530

    fixed the backend server to use ipV4

--- backend/index.js
+++ backend/index.js
@@ -162,9 +162,10 @@ const onlineDrivers = {};
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
 // Socket connection handling
 io.on('connection', (socket) => {
